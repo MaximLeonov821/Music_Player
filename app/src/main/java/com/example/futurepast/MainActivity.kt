c58364d13940410b460a0c88e226a3d80cd10cdf
MaximLeonov821 <maximleonov228@gmail.com>
2025-09-15 21:39:31 +0300

    В файл MainActivity добавлена базовая логика активности и свзяь между .kt и .xml файлом

--- app/src/main/java/com/example/futurepast/MainActivity.kt
+++ app/src/main/java/com/example/futurepast/MainActivity.kt
@@ -7,5 +7,11 @@ import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
-class MainActivity {
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?){
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_main)
+
+    }
 }
