8b3c9a76e220a2d8f651112996ff9bceaeb84677
MaximLeonov821 <maximleonov228@gmail.com>
2025-09-28 18:52:08 +0300

    Полный пересбор проекта и добавление viewBinding в проект

---
+++ app/src/main/java/com/example/futurepast/MainActivity.kt
@@ -0,0 +1,26 @@
+package com.example.futurepast
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import com.example.futurepast.databinding.ActivityPlayerBinding
+
+class MainActivity : AppCompatActivity() {
+    private var _playerActivity: ActivityPlayerBinding? = null
+    private val playerActivity get() = _playerActivity ?: throw IllegalStateException("Binding for ActivityLearnWordBinding must not be null")
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        _playerActivity = ActivityPlayerBinding.inflate(layoutInflater)
+        enableEdgeToEdge()
+        setContentView(playerActivity.root)
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        _playerActivity = null
+    }
+
+}
