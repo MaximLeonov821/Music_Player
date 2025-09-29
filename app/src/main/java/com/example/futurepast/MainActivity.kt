6cbdc842cfd2a92ad8c7e267631f372ccd408dab
MaximLeonov821 <maximleonov228@gmail.com>
2025-09-29 12:16:30 +0300

    добавлена логика переключения кнопок паузы и продолжения музыки. Также немного подправлены размеры и отсупы элементов навигации музыки

--- app/src/main/java/com/example/futurepast/MainActivity.kt
+++ app/src/main/java/com/example/futurepast/MainActivity.kt
@@ -10,13 +10,26 @@ import com.example.futurepast.databinding.ActivityPlayerBinding
 
 class MainActivity : AppCompatActivity() {
     private var _playerActivity: ActivityPlayerBinding? = null
-    private val playerActivity get() = _playerActivity ?: throw IllegalStateException("Binding for ActivityLearnWordBinding must not be null")
+    private val playerActivity get() = _playerActivity!!
 
+    private  var isPlaying = true
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         _playerActivity = ActivityPlayerBinding.inflate(layoutInflater)
         enableEdgeToEdge()
         setContentView(playerActivity.root)
+
+        playerActivity.PlayPauseSwitcher.setOnClickListener {
+            toggleSwitcher()
+        }
+    }
+    private fun toggleSwitcher() {
+        isPlaying = !isPlaying
+        if (!isPlaying) {
+            playerActivity.PlayPauseSwitcher.setImageResource(R.drawable.ic_pause)
+        } else {
+            playerActivity.PlayPauseSwitcher.setImageResource(R.drawable.ic_play)
+        }
     }
 
     override fun onDestroy() {
